f8f0746863e38d5a556a2337b903b780e3b0a19f
Sonava <sonava@code.edu.az>
2024-04-04 17:26:57 +0400

    Initial Commit - Front to Back

---
+++ PustokApp/Controllers/PagesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PustokApp.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult Compare()
+        {
+            return View();
+        }
+
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+
+        public IActionResult LoginRegister()
+        {
+            return View();
+        }
+
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+
+        public IActionResult OrderComplate()
+        {
+            return View();
+        }
+
+        public IActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
